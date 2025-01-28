a321454e33822c3a43da55240f879dd644a4b2f0
Sarah.bjt <sarah.bjlt@gmail.com>
2025-01-28 10:15:50 +0100

    surcharge possible move

--- src/piece.hpp
+++ src/piece.hpp
@@ -23,12 +23,12 @@ private:
 
 public:
     void
-                                     move(const std::pair<int, int>& newPosition); // Déplace la pièce
-    std::vector<std::pair<int, int>> possible_moves() const;                       // Retourne les déplacements possibles
-    std::pair<int, int>              get_position() const;                         // Récupère la position actuelle
-    std::string                      get_type() const;                             // Récupère le type de la pièce
-    State                            get_state() const;                            // Récupère l'état de la pièce
-    void                             set_state(State newState);                    // Modifie l'état de la pièce
+                                     move(const std::pair<int, int>& newPosition);  // Déplace la pièce
+    std::vector<std::pair<int, int>> possible_moves(const std::string& type) const; // Retourne les déplacements possibles
+    std::pair<int, int>              get_position() const;                          // Récupère la position actuelle
+    std::string                      get_type() const;                              // Récupère le type de la pièce
+    State                            get_state() const;                             // Récupère l'état de la pièce
+    void                             set_state(State newState);                     // Modifie l'état de la pièce
 
     // constructor
     Piece(const std::string& name, int color, const std::pair<int, int>& startPosition);
