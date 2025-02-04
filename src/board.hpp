be29843f9cc31d7d6427c914ee76b8b5cfdf0d8c
Agathe Jan <jan.ogathe@hotmail.com>
2025-02-04 10:20:12 +0100

    Changement de nom

--- src/board.hpp
+++ src/board.hpp
@@ -2,3 +2,14 @@
 #include <vector>
 #include "Piece.hpp"
 
+class ChessBoard {
+private:
+    std::vector<Piece*> board;
+
+public:
+    ChessBoard() : board(64, nullptr) {}
+
+    int position_to_index(int x, int y) const {
+        return y * 8 + x;
+    }
+}
