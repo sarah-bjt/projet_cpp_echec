0d8c26f1f9bb3dd89d84576bc511448c71dd3d1e
Sarah.bjt <sarah.bjlt@gmail.com>
2025-04-07 15:42:42 +0200

    avec les lib glm et max

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,15 @@
+
 #include <imgui.h>
+#include <glm/glm.hpp>
+#include <iostream>
+#include "Camera.hpp"
+#include "Model3D.hpp"
+#include "Shader.hpp"
 #include "board.hpp" // Inclure le fichier de la classe Board
-#include "quick_imgui/quick_imgui.hpp"
 #include "glad/glad.h"
+#include "glm/ext/matrix_clip_space.hpp"
+#include "glm/fwd.hpp"
+#include "quick_imgui/quick_imgui.hpp"
 
 int main()
 {
@@ -10,7 +18,7 @@ int main()
     quick_imgui::loop(
         "Chess",
         {
-            .init = [&]() {board.init();},
+            .init = [&]() { board.init(); },
             .loop =
                 [&]() {
                     // ImGui::ShowDemoWindow();
@@ -18,11 +26,11 @@ int main()
                     board.render();
                     ImGui::End();
                 },
-                .key_callback             = [](int key, int scancode, int action, int mods) { std::cout << "Key: " << key << " Scancode: " << scancode << " Action: " << action << " Mods: " << mods << '\n'; },
-                .mouse_button_callback    = [](int button, int action, int mods) { std::cout << "Button: " << button << " Action: " << action << " Mods: " << mods << '\n'; },
-                .cursor_position_callback = [](double xpos, double ypos) { std::cout << "Position: " << xpos << ' ' << ypos << '\n'; },
-                .scroll_callback          = [](double xoffset, double yoffset) { std::cout << "Scroll: " << xoffset << ' ' << yoffset << '\n'; },
-                .window_size_callback     = [](int width, int height) { std::cout << "Resized: " << width << ' ' << height << '\n'; },
+            .key_callback             = [](int key, int scancode, int action, int mods) { std::cout << "Key: " << key << " Scancode: " << scancode << " Action: " << action << " Mods: " << mods << '\n'; },
+            .mouse_button_callback    = [](int button, int action, int mods) { std::cout << "Button: " << button << " Action: " << action << " Mods: " << mods << '\n'; },
+            .cursor_position_callback = [](double xpos, double ypos) { std::cout << "Position: " << xpos << ' ' << ypos << '\n'; },
+            .scroll_callback          = [](double xoffset, double yoffset) { std::cout << "Scroll: " << xoffset << ' ' << yoffset << '\n'; },
+            .window_size_callback     = [](int width, int height) { std::cout << "Resized: " << width << ' ' << height << '\n'; },
         }
     );
     return 0;
